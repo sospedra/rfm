bee5eafd7bcc925fd9bf294fc80f2d61d8d2b52e
Rubén Sospedra <hello@sospedra.me>
2020-04-19 16:46:24 +0200

    test: Pass home suite

--- src/home/Home.spec.tsx
+++ src/home/Home.spec.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+import { HashRouter } from 'react-router-dom'
 import Home from './index'
 
 test('renders learn react link', () => {
-  const { getByText } = render(<Home />)
-  const linkElement = getByText(/learn react/i)
+  const { getByText } = render(
+    <HashRouter>
+      <Home />
+    </HashRouter>,
+  )
+  const linkElement = getByText(/Browse repos that need support/i)
   expect(linkElement).toBeInTheDocument()
 })
