e03cfb11948bd7748bd016361d0fc0d0a067dae2
Rubén Sospedra <hello@sospedra.me>
2020-05-03 15:18:24 +0200

    feat: Add Featured in PH badge

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -7,7 +7,6 @@ import { track } from '../rfm/services/analytics'
 import Search from './Search'
 import List from './List'
 import Newsletter from './Newsletter'
-import NewsletterBanner from './NewsletterBanner'
 
 const Home: React.FC<{}> = () => {
   const [query, setQuery] = useState(' ')
@@ -22,7 +21,19 @@ const Home: React.FC<{}> = () => {
   return (
     <Shell>
       <Search setQuery={setQuery} />
-      <NewsletterBanner />
+      <a
+        href='https://www.producthunt.com/posts/request-for-maintainers?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-request-for-maintainers'
+        className='justify-center w-full'
+        target='_blank'
+      >
+        <img
+          src='https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=195531&theme=dark'
+          alt='Request for maintainers - Find any OSS project calling for collaborators | Product Hunt Embed'
+          style={{ width: 250, height: 54 }}
+          width='250px'
+          height='54px'
+        />
+      </a>
       <Error error={error} />
       <List {...data} />
       <Newsletter />
